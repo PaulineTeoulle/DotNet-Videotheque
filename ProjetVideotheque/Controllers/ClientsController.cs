5f7a3b16004fb31aff6568da70c811c6f3b84d31
Pauline <pa.teoulle@gmail.com>
2021-12-12 17:25:17 +0100

    Commentaire ClientsController

--- ProjetVideotheque/Controllers/ClientsController.cs
+++ ProjetVideotheque/Controllers/ClientsController.cs
@@ -18,66 +18,55 @@ namespace ProjetVideotheque.Controllers
             _context = context;
         }
 
-
-
         // GET: Clients
+        // Recherche la searchString dans la table Client et renvoie la liste à la Vue
         public async Task<IActionResult> Index(string searchString)
         {
-
             var clients = from m in _context.Client
                           select m;
-
             if (!String.IsNullOrEmpty(searchString))
             {
-                // movies = movies.Where(s => s.NomFilm.Contains(searchString));  
                 clients = clients.Where(s => s.NomClient.Contains(searchString) ||
                                                 s.PrenomClient.Contains(searchString) ||
                                                 s.AdresseClient.Contains(searchString) ||
                                                 s.MailClient.Contains(searchString));
-
             }
-
             return View(await clients.ToListAsync());
         }
 
-
-
         // GET: Clients/Details/5
+        // Retourne les informations d'un client grâce à l'id passé en URL 
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var client = await _context.Client
-                .FirstOrDefaultAsync(m => m.Id == id);
-
+            Client client = await GetClientFromId(id);
 
             if (client == null)
             {
                 return NotFound();
             }
 
-
-
+            //Récupération des locations du client
             var locations = GetLocationsForOneClient(id);
             ViewData["Locations"] = locations;
-
-
 
             return View(client);
         }
 
+      
+
         // GET: Clients/Create
+        //Renvoie la vue de création de client
         public IActionResult Create()
         {
             return View();
         }
 
         // POST: Clients/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Création d'un nouveau client dans le contexte via les paramètres bindés
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,NomClient,PrenomClient,AdresseClient,MailClient")] Client client)
@@ -92,14 +81,15 @@ namespace ProjetVideotheque.Controllers
         }
 
         // GET: Clients/Edit/5
+        // Retourne les informations d'un client grâce à l'id passé en URL 
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var client = await _context.Client.FindAsync(id);
+            //Récupération du client grâce à l'id
+            Client client = await GetClientFromId(id);
             if (client == null)
             {
                 return NotFound();
@@ -108,8 +98,7 @@ namespace ProjetVideotheque.Controllers
         }
 
         // POST: Clients/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // Modification d'un client dans le contexte via les paramètres bindés
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,NomClient,PrenomClient,AdresseClient,MailClient")] Client client)
@@ -143,6 +132,7 @@ namespace ProjetVideotheque.Controllers
         }
 
         // GET: Clients/Delete/5
+        // Retourne les informations d'un client grâce à l'id passé en URL 
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -150,8 +140,8 @@ namespace ProjetVideotheque.Controllers
                 return NotFound();
             }
 
-            var client = await _context.Client
-                .FirstOrDefaultAsync(m => m.Id == id);
+            //Récupération du client grâce à l'id
+            Client client = await GetClientFromId(id);
             if (client == null)
             {
                 return NotFound();
@@ -161,11 +151,12 @@ namespace ProjetVideotheque.Controllers
         }
 
         // POST: Clients/Delete/5
+        // Suppression du client dans le context récupéré via l'id
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var client = await _context.Client.FindAsync(id);
+            Client client = await GetClientFromId(id);
             _context.Client.Remove(client);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -177,46 +168,38 @@ namespace ProjetVideotheque.Controllers
         }
 
         // GET: Clients/Facture
+        // Récupère les informations utiles à passer dans la vue pour la facture
         public async Task<IActionResult> Facture(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var client = await _context.Client
-                .FirstOrDefaultAsync(m => m.Id == id);
+            //Récupération du client depuis l'id
+            Client client = await GetClientFromId(id);
 
             if (client == null)
             {
                 return NotFound();
             }
 
+            //Récupération des locations du client
             var locations = GetLocationsForOneClient(id);
             ViewData["Locations"] = locations;
-
-
+            // Récupération du prix total pour les locations du client
             ViewBag.totalPrice = GetTotalPrice(locations);
             return View(client);
         }
 
-        private static double GetTotalPrice(IEnumerable<Location> locations)
+
+        //Récupère le client depuis l'id
+        private async Task<Client> GetClientFromId(int? id)
         {
-
-            if (locations != null)
-            {
-                double totalPrice = 0;
-                foreach (Location location in locations)
-                {
-
-                    totalPrice += location.LocationFilmId.PrixParJour * ((location.DateRetourLocation.Date - location.DateDebutLocation.Date).TotalDays);
-                }
-                return totalPrice;
-            }
-            return 0;
+            return await _context.Client
+                .FirstOrDefaultAsync(m => m.Id == id);
         }
 
-
+        //Récupère les locations en cours pour un client
         private IEnumerable<Location> GetLocationsForOneClient(int? id)
         {
 
@@ -229,12 +212,24 @@ namespace ProjetVideotheque.Controllers
                .Include(l => l.LocationClientId)
                .Include(l => l.LocationFilmId).ToList();
 
-
             if (!locations.Any()) return null;
             else return locations.Distinct();
         }
 
-
+        //Calcule le prix total des locations
+        private static double GetTotalPrice(IEnumerable<Location> locations)
+        {
+            if (locations != null)
+            {
+                double totalPrice = 0;
+                foreach (Location location in locations)
+                {
+                    totalPrice += location.LocationFilmId.PrixParJour * ((location.DateRetourLocation.Date - location.DateDebutLocation.Date).TotalDays);
+                }
+                return totalPrice;
+            }
+            return 0;
+        }
 
     }
 }
--- ProjetVideotheque/Startup.cs
+++ ProjetVideotheque/Startup.cs
@@ -16,7 +16,7 @@ namespace ProjetVideotheque
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -24,13 +24,9 @@ namespace ProjetVideotheque
             services.AddDbContext<Context>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Context")));
 
-
-
-
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,8 +36,7 @@ namespace ProjetVideotheque
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
